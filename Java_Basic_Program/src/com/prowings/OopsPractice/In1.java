5c01781b00d0448751e19c866b2e55bec9243713
pramodjadhav101095 <pramodjadhav101095@gmail.com>
2023-10-13 12:04:42 +0530

    oops

---
+++ Java_Basic_Program/src/com/prowings/OopsPractice/In1.java
@@ -0,0 +1,14 @@
+package com.prowings.OopsPractice;
+
+public interface In1 {
+	
+	
+	// public static final   by default
+	
+	  final int  x =10;
+	
+	// public abstract by default
+	  
+	  void display(int a) ;
+
+}
